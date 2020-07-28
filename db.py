bb2be9027814177806386e7664ba85e632271723
Bracha Ginsberger <gb@sem-wolf.org>
2020-07-29 00:39:52 +0300

    add create_table method to DataBase class

--- db.py
+++ db.py
@@ -68,3 +68,25 @@ class DBTable(db_api.DBTable):
                 bson_file.write(BSON.encode(keys_dict))
 
 
+
+class DataBase(db_api.DataBase):
+    __PATH = Path("db_files")
+    __TABLES = dict()
+    __BACKUP_PATH = Path("db_files_backup")
+
+    def create_table(self,
+                     table_name: str,
+                     fields: List[DBField],
+                     key_field_name: str) -> DBTable:
+
+        if key_field_name not in [field.name for field in fields]:
+            raise ValueError
+        try:
+            table_path = os.path.join(DataBase.__PATH, table_name)
+            os.mkdir(table_path)
+            new_table = DBTable(table_name, fields, key_field_name, table_path)
+            DataBase.__TABLES[table_name] = new_table
+            return new_table
+        except FileExistsError:
+            print(f"{table_name} is already exist")
+            return DataBase.__TABLES[table_name]
